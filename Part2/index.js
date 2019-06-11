51830548a5fb3832cf07960c1639d17ca2b41905
Wesley Walker <wesley0626@msn.com>
2019-06-11 09:01:42 -0600

    second_complete

---
+++ Part2/index.js
@@ -0,0 +1,6 @@
+let cardId = document.getElementById('cardId')
+let style = document.getElementById('style')
+let card = document.getElementById(cardId.value)
+function setCard(){
+  card.style.color = style.value 
+}
